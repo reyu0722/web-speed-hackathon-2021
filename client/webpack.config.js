aca3e328dc0ef1957e5c608f040aab8b4d159c88
reyu <ka15sugar@gmail.com>
2021-12-13 21:05:39 +0900

    production mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -59,7 +59,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: process.env.NODE_ENV || "production",
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
