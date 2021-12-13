1b9244cab6cc9d1d24b5ce438717bd419cf402fe
reyu <ka15sugar@gmail.com>
2021-12-13 23:46:41 +0900

    解像度の調整

--- public/convert-movie.sh
+++ public/convert-movie.sh
@@ -2,6 +2,6 @@ mkdir movies
 
 for file in `\find ./old-movies -maxdepth 1 -type f`; do
 	name=`basename $file .gif`
-	ffmpeg -f gif -i old-movies/${name}.gif -c vp9 -b:v 0 -crf 40 movies/${name}.webm
+	ffmpeg -f gif -i old-movies/${name}.gif -c vp9 -b:v 0 -crf 40 -vf scale=640:-1 movies/${name}.webm
 	echo ${name}
 done
