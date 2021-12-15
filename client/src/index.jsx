db0053bd6f8565820082594532afbcdc86731920
reyu <ka15sugar@gmail.com>
2021-12-15 20:34:57 +0900

    render before load

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { AppContainer } from './containers/AppContainer';
 
-window.addEventListener('load', () => {
-  ReactDOM.render(
-    <AppContainer />,
-    document.getElementById('app'),
-  );
-});
+ReactDOM.render(
+  <AppContainer />,
+  document.getElementById('app'),
+);
