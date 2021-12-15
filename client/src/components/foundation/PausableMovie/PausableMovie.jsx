2cbf27c1773c5256fea3b4f597db02959b67dca6
reyu <ka15sugar@gmail.com>
2021-12-16 00:07:31 +0900

    video autoplay

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -18,21 +18,7 @@ const PausableMovie = ({ src }) => {
 
   /** @type {React.Ref<HTMLVideoElement>} */
   const videoRef = useRef(null)
-  const [isPlaying, setIsPlaying] = useState(false)
-
-  useEffect(() => {
-    if (!videoRef.current) return
-
-    videoRef.current.pause();
-    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-      videoRef.current.pause();
-      setIsPlaying(false);
-    } else {
-      videoRef.current.play();
-      setIsPlaying(true);
-    }
-    return
-  }, [])
+  const [isPlaying, setIsPlaying] = useState(true)
 
   const handleClick = useCallback(() => {
     if (!videoRef.current) return
@@ -49,7 +35,7 @@ const PausableMovie = ({ src }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <video src={ src } ref={videoRef} className="w-full" />
+        <video src={ src } ref={videoRef} className="w-full" autoplay muted />
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
